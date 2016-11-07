ef034fa111a96ad6abd46ff484528393c2fc4d50
Jens Andre Kaalen <ja.kaalen@gmail.com>
2016-11-07 20:03:31 +0100

    cleaned up

--- app/test.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'test-component',
-    templateUrl: 'test.component.html'
-})
-export class TestComponent {
-    testText: string = "this is the initial text";
-}
